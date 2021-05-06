5dcabe39fac6c15f28f7adafd110ae113d53bf7c
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2021-05-06 22:59:45 +0900

    おれおれチェックボックス

--- checkbox-sandbox/main.js
+++ checkbox-sandbox/main.js
@@ -1,3 +1,21 @@
-import 'minireset.css'
-import './style.css'
+import "minireset.css";
+import "./style.css";
 
+const checkboxList = document.querySelectorAll(".my-checkbox");
+for (const c of checkboxList) {
+  applyCheckbox(c);
+}
+
+function applyCheckbox(myCheckbox) {
+  const checkInput = myCheckbox.querySelector(`input[type="checkbox"]`);
+  const checkMark = myCheckbox.querySelector(`.check-mark`);
+
+  checkInput.addEventListener("input", () => {
+    const check = checkInput.checked;
+    if (check) {
+      checkMark.classList.add("is-checked");
+    } else {
+      checkMark.classList.remove("is-checked");
+    }
+  });
+}
